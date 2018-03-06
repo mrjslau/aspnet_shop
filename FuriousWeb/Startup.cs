56c9ed4d659437cea42fbc92cb7de1f4ab1aa1f6
hiroshi <hiroshi@HIROSHI-PC>
2018-03-06 12:16:09 +0200

    Add project files.

---
+++ FuriousWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FuriousWeb.Startup))]
+namespace FuriousWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
