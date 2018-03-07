925ebb6d53216ee63d864acaa3e70f232d9f4ec1
Gabrielius <gabrielius.petrauskas@gmail.com>
2018-03-07 21:08:58 +0200

    gabrieliaus test commit

--- FuriousWeb/Startup.cs
+++ FuriousWeb/Startup.cs
@@ -8,7 +8,7 @@ namespace FuriousWeb
     {
         public void Configuration(IAppBuilder app)
         {
-            ConfigureAuth(app);
+            ConfigureAuth(app);//Gabrielius redaguoja
         }
     }
 }
