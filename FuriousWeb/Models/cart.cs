1681f6ae44a38ce6dc4dfe503cf13d8fbc598a5a
Gabrielius <gabrielius.petrauskas@gmail.com>
2018-03-11 20:56:12 +0200

    cart

---
+++ FuriousWeb/Models/cart.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FuriousWeb.Models
+{
+    public class Cart
+    {
+
+        /*public void ExecutePayment(double amount, string number, string holder, int exp_year, int exp_month, string cvv)
+        {
+            var payment = new
+            {
+                amount = amount,
+                number = number,
+                holder = holder,
+                exp_year = exp_year,
+                exp_month = exp_month,
+                cvv = cvv
+            };
+
+            var json = JsonConvert.SerializeObject(payment);
+
+        }*/
+    }
+}
