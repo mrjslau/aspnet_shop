ba75432a28acc50f498bad3e35d402131704ce97
Glebas <glebko21@gmail.com>
2018-04-25 23:43:42 +0300

    SQL scriptai darbui su DB. kol kas niekur neiškviečiami (tai galima padaryt su ranka, atidarius scripta bei paspaudus 'Run'). Vėliau sukursiu jiem '.BAT' faila.

---
+++ FuriousWeb/Data/SQL/Views/DetailedProducts.View.sql
@@ -0,0 +1,3 @@
+﻿CREATE VIEW DetailedProducts
+	AS SELECT ProductId, Code, Name, Description, WarehouseId, Quantity, Price 
+	FROM Products INNER JOIN Stock ON Products.Id = ProductId
---
+++ FuriousWeb/Data/SQL/db_fill.sql
@@ -0,0 +1,21 @@
+﻿INSERT INTO Warehouses (Code, Name, Address)
+VALUES 
+('WR1', 'Pagrindinis Sandelis', 'Naugarduko 48'),
+('WR2', 'Atsarginis Sandelis', 'Naugarduko 49');
+
+INSERT INTO Products(Code, Name, Description)
+VALUES 
+('PR1', 'Prekė 1', 'Prekės 1 aprašymas..'),
+('PR2', 'Prekė 2', 'Prekės 2 aprašymas'),
+('PR3', 'Prekė 3', 'Prekės 3 aprašymas..'),
+('PR4', 'Prekė 4', 'Prekės 4 aprašymas'),
+('PR5', 'Prekė 5', 'Prekės 5 aprašymas..'),
+('PR6', 'Prekė 6', 'Prekės 6 aprašymas');
+
+INSERT INTO Stock(WarehouseId, ProductId, Price, Quantity)
+VALUES 
+(1, 1, 5.99, 100),
+(1, 2, 5.99, 100),
+(1, 3, 13.99, 10),
+(1, 4, 24.65, 10),
+(1, 5, 119.99, 5);
