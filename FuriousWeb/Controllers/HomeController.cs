a30074f68f350d3bb0cdf22c3fcba12c8f4e4f3b
Glebas <glebko21@gmail.com>
2018-05-01 20:18:33 +0300

    namespace pataisymai view'uose

--- FuriousWeb/Controllers/HomeController.cs
+++ FuriousWeb/Controllers/HomeController.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using FuriousWeb.Models;
-using System.Web.Http.Cors;
 
 namespace FuriousWeb.Controllers
 {
