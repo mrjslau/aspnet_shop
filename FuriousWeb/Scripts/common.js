4c6266e7c62916d1d3bb066217f66e6e491e8899
Glebas <glebko21@gmail.com>
2018-05-13 15:48:20 +0300

    Prekių krepšelis - prekės išėmimas ir kiekio redagavimas; common.js
    Related Work Items: #32

---
+++ FuriousWeb/Scripts/common.js
@@ -0,0 +1,33 @@
+﻿function onBtnAddToCartClick(el, productId, refreshCart) {
+    var productQuantity = parseInt($(el).siblings(".productQuantity").val());
+    $.ajax({
+        type: "GET",
+        url: '/ShoppingCart/AddItem',
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        async: true,
+        data: { productId: productId, quantity: productQuantity },
+        error: function (xhr, status, errorThrown) {
+            var errorMsg = "Status: " + status + " " + errorThrown;
+            console.log(errorMsg);
+            alert(errorMsg);
+        },
+        success: function (response) {
+            if (response.success) {
+                $("#shoppingCartItemsCount").text(response.shoppingCartItemsCount);
+
+                if (refreshCart) {
+                    var oldQuantity = parseInt($('.quantity').text());
+                    $('.quantity').text(oldQuantity + productQuantity);
+                }
+                alert("Prekė buvo sėkmingai įkelta į krėpšelį");
+            }
+        }
+    })
+}
+
+//neleidžiam įvesti mažiau už 1
+function onProductQuantityChange(element) {
+    if ($(element).val() < 1)
+        $(element).val(1);
+}
