de1f24208b2193e6a9ea144f69fb1aa2ff0864a5
Glebas <glebko21@gmail.com>
2018-05-31 16:42:47 +0300

    pirkimo validacija

--- FuriousWeb/Controllers/CheckoutController.cs
+++ FuriousWeb/Controllers/CheckoutController.cs
@@ -1,5 +1,6 @@
 ﻿using FuriousWeb.Data;
 using FuriousWeb.Models;
+using FuriousWeb.Models.ViewModels;
 using Microsoft.AspNet.Identity;
 using System;
 using System.Web.Mvc;
@@ -12,15 +13,17 @@ namespace FuriousWeb.Controllers
         private DatabaseContext db = new DatabaseContext();
 
         public ActionResult OpenCheckout()
-        {
-            Checkout checkout = new Checkout();
+        {           
             bool loggedIn = (System.Web.HttpContext.Current.User != null) && System.Web.HttpContext.Current.User.Identity.IsAuthenticated;
             if (loggedIn)
             {
                 var user = System.Web.HttpContext.Current.User.Identity.GetUserId();
-                checkout.UserID = user;
-                checkout.User = db.Users.Find(user);
-                return View("Checkout", checkout);
+
+                var viewModel = new CheckoutViewModel();
+                viewModel.UserID = user;
+                viewModel.User = db.Users.Find(user);
+
+                return View("Checkout", viewModel);
             }
             else
             {
@@ -30,65 +33,72 @@ namespace FuriousWeb.Controllers
             }
         }
 
-        public ActionResult SubmitPayment()
+        public ActionResult SubmitPayment(CheckoutViewModel viewModel)
         {
-            var user = User.Identity.GetUserName();
-            Checkout checkout = new Checkout()
+            if (ModelState.IsValid)
             {
-                Card_number = Request.Form["card_number"],
-                Exp_year = Int32.Parse(Request.Form["exp_year"]),
-                Exp_month = Int32.Parse(Request.Form["exp_month"]),
-                Card_holder = user,
-                Card_cvv = Request.Form["card_cvv"],
-
-                UserID = User.Identity.GetUserId(),
-            };
-            
-            if (checkout.InitPayment((ShoppingCart)HttpContext.Session["shoppingCart"]))
-            {
-                //save payment
-                var payment = new Payment();
-                payment.Amount = checkout.paymentInfo.Amount;
-                payment.Created_at = checkout.paymentInfo.Created_at;
-                payment.Code = checkout.paymentInfo.Id;
-                db.Payments.Add(payment);
-
-                //save order
-                var order = new Order();
-                order.PaymentID = payment.ID;
-                order.UserID = checkout.UserID;
-                var date = DateTime.Parse(checkout.paymentInfo.Created_at);
-                order.Created_at = date.ToString("yyyy-MM-dd");
-                order.Status = 1;
-                db.Orders.Add(order);
-
-                //save order details
-                foreach(ShoppingCartItem item in checkout.Cart.GetItems())
+                var user = User.Identity.GetUserName();
+                Checkout checkout = new Checkout()
                 {
-                    var orderDetail = new OrderDetail();
-                    orderDetail.OrderID = order.ID;
-                    orderDetail.ProductID = item.Product.Id;
-                    orderDetail.Quantity = item.Quantity;
-                    db.OrderDetails.Add(orderDetail);
-                }
-                try
-                {
-                    db.SaveChanges();
-                }
-                catch(Exception ex)
-                {
-                    ModelState.AddModelError("Error", ex.Message);
-                    return View("Checkout", checkout);
-                }
-                HttpContext.Session["shoppingCartItemsCount"] = 0;
-                HttpContext.Session["shoppingCart"] = new ShoppingCart();
+                    Card_number = viewModel.Card_number,
+                    Exp_year = viewModel.Exp_year,
+                    Exp_month = viewModel.Exp_month,
+                    Card_holder = user,
+                    Card_cvv = viewModel.Card_cvv,
 
-                return View("Thank-you");
+                    UserID = User.Identity.GetUserId(),
+                };
+
+                if (checkout.InitPayment((ShoppingCart)HttpContext.Session["shoppingCart"]))
+                {
+                    //save payment
+                    var payment = new Payment();
+                    payment.Amount = checkout.paymentInfo.Amount;
+                    payment.Created_at = checkout.paymentInfo.Created_at;
+                    payment.Code = checkout.paymentInfo.Id;
+                    db.Payments.Add(payment);
+
+                    //save order
+                    var order = new Order();
+                    order.PaymentID = payment.ID;
+                    order.UserID = checkout.UserID;
+                    var date = DateTime.Parse(checkout.paymentInfo.Created_at);
+                    order.Created_at = date.ToString("yyyy-MM-dd");
+                    order.Status = 1;
+                    db.Orders.Add(order);
+
+                    //save order details
+                    foreach (ShoppingCartItem item in checkout.Cart.GetItems())
+                    {
+                        var orderDetail = new OrderDetail();
+                        orderDetail.OrderID = order.ID;
+                        orderDetail.ProductID = item.Product.Id;
+                        orderDetail.Quantity = item.Quantity;
+                        db.OrderDetails.Add(orderDetail);
+                    }
+                    try
+                    {
+                        db.SaveChanges();
+                    }
+                    catch (Exception ex)
+                    {
+                        ModelState.AddModelError("Error", ex.Message);
+                        return View("Checkout", viewModel);
+                    }
+                    HttpContext.Session["shoppingCartItemsCount"] = 0;
+                    HttpContext.Session["shoppingCart"] = new ShoppingCart();
+
+                    return View("Thank-you");
+                }
+                else
+                {
+                    viewModel.paymentErr = checkout.paymentErr;
+                    return View("Checkout", viewModel);
+                }
             }
-            else
-            {
-                return View("Checkout", checkout);
-            }
+
+            //!ModelState.IsValid
+            return View("Checkout", viewModel);
         }
     }
 }
---
+++ FuriousWeb/Models/ViewModels/CheckoutViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FuriousWeb.Models.ViewModels
+{
+    public class CheckoutViewModel
+    {
+        [Required(ErrorMessage = "Būtina įvesti kortelės numerį")]
+        public string Card_number { get; set; }
+        [Required(ErrorMessage = "Būtina įvesti metus")]
+        public int Exp_year { get; set; }
+        [Required(ErrorMessage = "Būtina įvesti mėnesį")]
+        public int Exp_month { get; set; }
+        [Required(ErrorMessage = "Būtina įvesti mėnesį")]
+        public string Card_cvv { get; set; }
+
+        [Required(ErrorMessage = "Būtina įvesti pavardę")]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "Būtina įvesti pavardę")]
+        public string LastName { get; set; }
+
+        public PaymentError paymentErr { get; set; }
+
+        public string UserID { get; set; }
+        public User User { get; set; }
+    }
+}
