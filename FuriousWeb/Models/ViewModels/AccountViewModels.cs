1a3fd27749a28902a58a7a11d624223061b8fad4
Glebas <glebko21@gmail.com>
2018-05-31 15:21:11 +0300

    ismestas nenaudojamas kodas

--- FuriousWeb/Models/ViewModels/AccountViewModels.cs
+++ FuriousWeb/Models/ViewModels/AccountViewModels.cs
@@ -88,33 +88,4 @@ namespace FuriousWeb.Models.ViewModels
         [Display(Name = "Pavardė")]
         public string Lastname { get; set; }
     }
-
-    public class ResetPasswordViewModel
-    {
-        [Required]
-        [EmailAddress]
-        [Display(Name = "Email")]
-        public string Email { get; set; }
-
-        [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 8)]
-        [DataType(DataType.Password)]
-        [Display(Name = "Password")]
-        public string Password { get; set; }
-
-        [DataType(DataType.Password)]
-        [Display(Name = "Confirm password")]
-        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
-        public string ConfirmPassword { get; set; }
-
-        public string Code { get; set; }
-    }
-
-    public class ForgotPasswordViewModel
-    {
-        [Required]
-        [EmailAddress]
-        [Display(Name = "Email")]
-        public string Email { get; set; }
-    }
 }
